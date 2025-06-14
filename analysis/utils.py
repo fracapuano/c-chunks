f4f0d1b60c6b4ea1f14e44192f59ee26628cb9e0
Francesco Capuano <francesco_capuano@aol.com>
2025-06-15 00:10:14 +0200

    fix: iterate on visuals

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,8 +1,18 @@
 import torch
-from datasets import load_dataset
 import numpy as np
 from sklearn.decomposition import PCA
 from tqdm import tqdm
+import lpips
+
+lpips_alexnet = lpips.LPIPS(net='alex')
+
+@torch.no_grad()
+def perceptual_difference(image1: torch.Tensor, image2: torch.Tensor) -> torch.Tensor:
+    assert image1.ndim == image2.ndim == 3, "Images are supposed to be (RGB)!"
+    return lpips_alexnet(image1, image2).item()
+
+perceptual_keys = ["obs_observation_images_top", "obs_observation_images_wrist"]
+vector_keys = ["obs_observation_state"]
 
 
 def chunks_difference(chunk1: torch.Tensor, chunk2:torch.Tensor) -> float:
@@ -39,22 +49,35 @@ def reduce_chunks_dim(dataset):
 
     return pca
 
-def get_chunk_pairs_differences(dataset, n_chunk_in_pairs: int = 2):
 
-    differences = torch.zeros(
-        (len(dataset)-(n_chunk_in_pairs-1), n_chunk_in_pairs-1)
+def compute_frames_chunks_differences(dataset, n_chunk_in_pairs: int = 2):
+    assert n_chunk_in_pairs == 2, "Perceptual difference is not implemented for more than 2 pairs!"
+    
+    chunks_differences = torch.zeros(
+        len(dataset)-(n_chunk_in_pairs-1)
     )
+    frames_differences = []
 
     for idx in tqdm(range(len(dataset)-(n_chunk_in_pairs-1))):
         rows = dataset[idx:idx+n_chunk_in_pairs]
         aligned_chunks = align_chunks(rows["action_chunks"])
+
+        frames_diff = {}
+        for p_key in perceptual_keys:
+            images = rows[p_key]
+            frames_diff[p_key] = perceptual_difference(images[0, :], images[1, :])
+        
+        for v_key in vector_keys:
+            vectors = rows[v_key]
+            frames_diff[v_key] = torch.linalg.norm(vectors[0, :] - vectors[1, :]).item()
+        
+        frames_diff["avg"] = torch.tensor([frames_diff[k] for k in [*vector_keys, *perceptual_keys]]).mean().item()
+        frames_differences.append(frames_diff)
         
         for col in range(1, n_chunk_in_pairs):
             chunks_diff = chunks_difference(aligned_chunks[0], aligned_chunks[col])
             
-            if differences.shape[1] != 1:
-                differences[idx, col] = chunks_diff
-            else:
-                differences[idx, 0] = chunks_diff
+            chunks_differences[idx] = chunks_diff
 
-    return differences
+    return chunks_differences, frames_differences
+
